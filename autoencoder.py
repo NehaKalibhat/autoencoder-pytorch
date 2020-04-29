74f470959e89cc4a09d03460eed2e3100bc4c8c0
Neha Kalibhat <nehamk@cml06.umiacs.umd.edu>
2020-04-29 11:35:43 -0400

    argparser changes

--- autoencoder.py
+++ autoencoder.py
@@ -13,21 +13,6 @@ import pdb
 import matplotlib.pyplot as plt
 import argparse
 
-num_epochs = 100
-batch_size = 128
-learning_rate = 1e-3
-
-img_transform = transforms.Compose([
-            transforms.Resize(32),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,))
-        ])
-
-#dataset = CIFAR10('../datasets/cifar', download=True, transform=img_transform)
-dataset = MNIST('../datasets/mnist', download=True, transform=img_transform)
-dataloader1 = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
 
 class autoencoder(nn.Module):
     '''
@@ -60,38 +45,35 @@ class autoencoder(nn.Module):
     def __init__(self):
         super(autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 128, 3, stride=1, padding=1),  # b, 256, 32, 32
+            nn.Conv2d(3, ndf, 3, stride=1, padding=1),  # b, 256, 32, 32
             nn.ReLU(True),
-             nn.Conv2d(128, 64, 4, stride=2, padding=1),  # b, 96, 16, 16
+            nn.Conv2d(ndf, ndf * 2, 4, stride=2, padding=1),  # b, 96, 16, 16
             nn.ReLU(True),
-            nn.Conv2d(64, 32, 2, stride=2, padding=2),  # b, 48, 10, 10
+            nn.Conv2d(ndf * 2, ndf * 4, 2, stride=2, padding=2),  # b, 48, 10, 10
             nn.ReLU(True),
-            nn.Conv2d(32, 16, 3, stride=1, padding=1),  # b, 24, 10, 10
+            nn.Conv2d(ndf * 4, ndf * 8, 3, stride=1, padding=1),  # b, 24, 10, 10
             nn.ReLU(True),
             nn.MaxPool2d(2, stride=2),  # b, 12, 5, 5
-            nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 12, 3, 3
+            nn.Conv2d(ndf * 8, ndf * 16, 3, stride=2, padding=1),  # b, 12, 3, 3
             nn.ReLU(True),
             nn.MaxPool2d(2, stride=1)  # b, 12, 2, 2
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(8, 128, 3, stride=2),  # b, 24, 5, 5
+            nn.ConvTranspose2d(ndf * 16, ndf * 8, 3, stride=2),  # b, 24, 5, 5
             nn.ReLU(True),
-            nn.ConvTranspose2d(128, 64, 2, stride=2),  # b, 48, 10, 10
+            nn.ConvTranspose2d(ndf * 8, ndf * 4, 2, stride=2),  # b, 48, 10, 10
             nn.ReLU(True),
-            nn.ConvTranspose2d(64, 32, 2, stride=2, padding=1),  # b, 12, 18, 18
+            nn.ConvTranspose2d(ndf * 4, ndf * 2, 2, stride=2, padding=1),  # b, 12, 18, 18
             nn.ReLU(True),
-            nn.ConvTranspose2d(32, 16, 5, stride=1, padding=1),  # b, 8, 20, 20
+            nn.ConvTranspose2d(ndf * 2, ndf, 5, stride=1, padding=1),  # b, 8, 20, 20
             nn.ReLU(True),
-            nn.ConvTranspose2d(16, 1, 2, stride=2, padding=4),  # b, 1, 32, 32
+            nn.ConvTranspose2d(ndf, 3, 2, stride=2, padding=4),  # b, 1, 32, 32
             nn.Tanh()
         )
         self.encoder = self.encoder.cuda()
         self.decoder = self.decoder.cuda()
         self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate, weight_decay=1e-5)
         self.criterion = nn.MSELoss()
-        fh = open(path + "/train.log", 'w')
-        fh.write('Logging')
-        fh.close()
         
     def forward(self, x):
         x = self.encoder(x)
@@ -112,13 +94,23 @@ class autoencoder(nn.Module):
         model_state['optimizer'] = self.optimizer.state_dict()
         torch.save(model_state, model_path)
         
-    def load_autoencoder(self, model_path):
+    def load_autoencoder(self, model_path, prune_encoder = True, prune_decoder = True):
         self.log(f'Loading saved autoencoder named {model_path}') 
         model_state = torch.load(model_path)
-        self.load_state_dict(model_state['autoencoder'])
+        new_state = {}
+        for name in model_state['autoencoder']:
+            if ("encoder" in name and prune_encoder) or ("decoder" in name and prune_decoder):
+                new_state[name] = model_state['autoencoder'][name]
+            else:
+                new_state[name] = self.state_dict()[name]
+        self.load_state_dict(new_state)
         self.optimizer.load_state_dict(model_state['optimizer'])
     
-    def one_shot_prune(self, pruning_perc, layer_wise = False, trained_original_model_state = None):
+    def one_shot_prune(self, pruning_perc, 
+                       layer_wise = False, 
+                       trained_original_model_state = None,
+                      prune_encoder = True,
+                      prune_decoder = True):
         self.log(f"************pruning {pruning_perc} of the network****************")
         self.log(f"Layer-wise pruning = {layer_wise}")
         model = None
@@ -136,19 +128,20 @@ class autoencoder(nn.Module):
 
             zeros = 0
             total = 0
-            self.log("Autoencoder layer-wise pruning percentages")
+            self.log("VAE layer-wise pruning percentages")
             for name in model:
-                #if "weight" in name:
-                weight_copy = model[name].data.abs().clone()
-                threshold = global_threshold
-                if layer_wise:
-                    layer_weights = model[name].data.cpu().numpy()
-                    threshold = np.percentile(abs(layer_weights), pruning_perc)
-                mask = weight_copy.gt(threshold).int()
-                self.log(f'{name} : {mask.numel() - mask.nonzero().size(0)} / {mask.numel()}  {(mask.numel() - mask.nonzero().size(0))/mask.numel()}')
-                zeros += mask.numel() - mask.nonzero().size(0)
-                total += mask.numel()
-                masks[name] = mask
+                if ("encoder" in name and prune_encoder) or ("decoder" in name and prune_decoder):
+                    threshold = global_threshold
+                    if layer_wise:
+                        layer_weights = model[name].data.cpu().numpy()
+                        threshold = np.percentile(abs(layer_weights), pruning_perc)
+                    masks[name] = model[name].gt(threshold).int()
+                    pruned = masks[name].numel() - masks[name].nonzero().size(0)
+                    tot = masks[name].numel()
+                    frac = pruned / tot
+                    self.log(f"{name} : {pruned} / {tot}  {frac}")
+                    zeros += pruned
+                    total += tot
             self.log(f"Fraction of weights pruned = {zeros}/{total} = {zeros/total}")
             self.masks = masks
     
@@ -173,7 +166,9 @@ class autoencoder(nn.Module):
                         trained_original_model_state, 
                         number_of_iterations, 
                         percent = 20, 
-                        init_with_old = True):
+                        init_with_old = True,
+                        prune_encoder = True,
+                        prune_decoder = True):
         
         
         weight_fractions = self.get_weight_fractions(number_of_iterations, percent)       
@@ -181,35 +176,46 @@ class autoencoder(nn.Module):
         for pruning_iter in range(0, number_of_iterations):
             self.log("Running pruning iteration {}".format(pruning_iter))
             self.__init__()
-            
+            self = self.cuda()
             trained_model = trained_original_model_state
             if pruning_iter != 0:
                 trained_model = path + "/"+ "end_of_" + str(pruning_iter - 1) + '.pth'
             
-            self.one_shot_prune(weight_fractions[pruning_iter], trained_original_model_state = trained_model)
-            model.train(prune = True, init_state = init_state, init_with_old = init_with_old)
+            self.one_shot_prune(weight_fractions[pruning_iter], 
+                                trained_original_model_state = trained_model, 
+                                prune_encoder = prune_encoder,
+                                prune_decoder = prune_decoder)
+            self.train(prune = True, 
+                       init_state = init_state, 
+                       init_with_old = init_with_old,
+                       prune_encoder = prune_encoder,
+                       prune_decoder = prune_decoder)
+            
             torch.save(self.state_dict(), path + "/"+ "end_of_" + str(pruning_iter) + '.pth')
             
             sample = self.forward(test_input.cuda())
-            save_image(sample * 0.5 + 0.5, path + '/image_' + str(pruning_iter) + '.png')
-
+            save_image(sample, path + '/image_' + str(pruning_iter) + '.png')
+            torch.cuda.empty_cache()
+            
         self.log("Finished Iterative Pruning")
         
         
-    def mask_gan(self):
+    def mask_gan(self, prune_encoder = True, prune_decoder = True):
         model = self.state_dict()
         for name in model:
-            #if "weight" in name:
-            model[name].data.mul_(self.masks[name])
+            if ("encoder" in name and prune_encoder) or ("decoder" in name and prune_decoder):
+                model[name].data.mul_(self.masks[name])
         self.load_state_dict(model)
     
-    def train(self, prune, init_state, init_with_old):
+    def train(self, prune, init_state, init_with_old, prune_encoder = True, prune_decoder = True):
         self.log(f"Number of parameters in model {sum(p.numel() for p in model.parameters())}")
         if not prune:
-            model.save_autoencoder('./mnist/before_train.pth')
+            model.save_autoencoder(path + '/before_train.pth')
         
         if prune and init_with_old:
-            model.load_autoencoder(init_state)
+            model.load_autoencoder(init_state, 
+                                  prune_encoder = prune_encoder,
+                                  prune_decoder = prune_decoder)
 
         for epoch in range(num_epochs):
             for data in dataloader:
@@ -223,33 +229,108 @@ class autoencoder(nn.Module):
                 loss.backward()
                 self.optimizer.step()
                 if prune:
-                    self.mask_gan()
+                    self.mask_gan(prune_encoder = prune_encoder,
+                                  prune_decoder = prune_decoder)
 
             # ===================log========================
             self.log('epoch [{}/{}], loss:{:.4f}'
                   .format(epoch + 1, num_epochs, loss.data.item()))
-            '''
-            if epoch % 10 == 0 or epoch == num_epochs - 1:
+            
+            if epoch == num_epochs - 1:
                 #save_image(torch.cat((output, img), 0) * 0.5 + 0.5, './cifar/image_{}.png'.format(epoch))
                 sample = self.forward(test_input.cuda())
                 save_image(sample * 0.5 + 0.5, path + '/image_{}.png'.format(epoch))
-            '''
+        
         torch.save(self.state_dict(), path + '/autoencoder.pth')
         
         
+parser = argparse.ArgumentParser(description='PyTorch Autoencoder')
+
+parser.add_argument('--num_epochs', default=100, type=int)
+parser.add_argument('--batch_size', default=128, type=int)
+parser.add_argument('--lr', default=1e-3, type=float)
+parser.add_argument('--image_size', default=32, type=int)
+parser.add_argument('--ndf', default=16, type=int)
+parser.add_argument('--dataset', default='../datasets/cifar', type=str)
+parser.add_argument('--inception_cache_path', default='./inception_cache/cifar', type=str)
+parser.add_argument('--log_path', default='./cifar_random_iter_3', type=str)
+parser.add_argument('--init_state', default='./cifar_1/before_train.pth', type=str)
+parser.add_argument('--trained_original_model_state', default='./cifar_1/autoencoder.pth', type=str)
+parser.add_argument('--init_with_old', default='True', type=str)
+parser.add_argument('--prune_encoder', default='True', type=str)
+parser.add_argument('--prune_decoder', default='True', type=str)
+   
+args = parser.parse_args()
+
+if args.num_epochs != '':
+     num_epochs = args.num_epochs
+        
+if args.batch_size != '':
+     batch_size = args.batch_size
+        
+if args.lr != '':
+     learning_rate = args.lr
+        
+if args.image_size != '':
+     image_size = args.image_size
+        
+if args.ndf != '':
+     ndf = args.ndf
+
+if args.dataset != '':
+     dataset = args.dataset
+        
+if args.inception_cache_path != '':
+     inception_cache_path = args.inception_cache_path
+
+if args.log_path != '':
+     path = args.log_path
+        
+if args.init_state != '':
+     init_state = args.init_state
+
+if args.trained_original_model_state != '':
+     trained_original_model_state = args.trained_original_model_state
+
+if args.init_with_old != '':
+     init_with_old = args.init_with_old == 'True'
+        
+if args.prune_encoder != '':
+     prune_encoder = args.prune_encoder == 'True'
+        
+if args.prune_decoder != '':
+     prune_decoder = args.prune_decoder == 'True'
+ 
+        
+img_transform = transforms.Compose([
+            transforms.Resize(image_size),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+        ])
+
+if 'cifar' in dataset:
+    dataset = CIFAR10(dataset, download=True, transform=img_transform)
+if 'mnist' in dataset:
+    dataset = MNIST(dataset, download=True, transform=img_transform)
+
+dataloader1 = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
 test_input, classes = next(iter(dataloader1)) 
 print(classes)
-    
-path = './mnist_random_iter'
-init_state = './mnist/before_train.pth'
-trained_original_model_state = './mnist/autoencoder.pth'
-init_with_old = False
+
+fh = open(path + "/train.log", 'w')
+fh.write('Logging')
+fh.close()
+        
 model = autoencoder()
 #model.one_shot_prune(98, trained_original_model_state = trained_original_model_state)
 model.iterative_prune(init_state = init_state, 
                     trained_original_model_state = trained_original_model_state, 
                     number_of_iterations = 20, 
                     percent = 20, 
-                    init_with_old = init_with_old)
-#model.train(prune = True, init_state = init_state, init_with_old = init_with_old)
+                    init_with_old = init_with_old,
+                    prune_encoder = prune_encoder,
+                    prune_decoder = prune_decoder)
+#model.train(prune = False, init_state = init_state, init_with_old = init_with_old)
 
